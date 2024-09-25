244b9032e1531c018b9f0933b438920c61d4fe3a
danshilxi <lianabriceno@my.tridenttech.edu>
2024-09-24 22:25:35 -0400

    Update lab4-2.cpp

--- lab4-2.cpp
+++ lab4-2.cpp
@@ -1,15 +1,25 @@
 //Lab4-2.cpp - displays a salesperson's commission
-//Philip Regalbuto
+//Created/revised by <your name> on <current date>
+
+
+
 #include <iostream>
 using namespace std;
-int main()
-{
 
+int main() 
+{ 
+  //declare named constant and variables
+  const double COMM_RATE = 0.1;
+  double sales = 12.1;
+  double commission = 0.0;
+
+  //enter input item
+  cout << "Sales amount: " ;
+  cin >> sales;
+
+  //calculate  and display the commission
+  commission = sales * COMM_RATE;
+  cout << "Commission: $" << commission << endl;
 
   return 0;
 } //end of main function
-/*
-Sales amount: 1328.50
-Commission: $132.85
-Press any key to continue . . .
-*/
